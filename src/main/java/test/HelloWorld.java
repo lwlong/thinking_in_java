f9ff590150847d275a67c57143ec0e35452a7f9d
liwenlong <liwenlong@wecash.net>
2017-04-25 21:08:38 +0800

    first commit

---
+++ src/main/java/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Lwlong on 2017/4/25.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
