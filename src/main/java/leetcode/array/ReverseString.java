c0971fd7363f395beff81e917373298055444983
LWL <lwlong2018@gmail.com>
2020-06-01 22:08:45 +0800

    update leetcode

---
+++ src/main/java/leetcode/array/ReverseString.java
@@ -0,0 +1,26 @@
+package leetcode.array;
+
+/**
+ *  https://leetcode.com/problems/reverse-string/
+ *  344. Reverse String
+ */
+public class ReverseString {
+    public static void main(String[] args) {
+
+        char[] s = {'h','e','l','l','o','o'};
+        System.out.println(s);
+        reverseString(s);
+        System.out.println(s);
+    }
+
+    public static void reverseString(char[] s) {
+
+        int length = s.length;
+        for(int i=0;i<length/2;i++){
+            char tmp = s[i];
+            s[i] = s[length-i-1];
+            s[length-i-1] = tmp;
+        }
+
+    }
+}
