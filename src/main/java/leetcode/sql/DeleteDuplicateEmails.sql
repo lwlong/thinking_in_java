706f0d149462a9bc1622dbb0b2c288bffd1c990d
LWL <lwlong2018@gmail.com>
2020-06-04 22:51:16 +0800

    update leetcode

---
+++ src/main/java/leetcode/sql/DeleteDuplicateEmails.sql
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/delete-duplicate-emails/
+196. Delete Duplicate Emails
+*/
+DELETE p1 FROM Person p1,
+    Person p2
+WHERE
+    p1.Email = p2.Email AND p1.Id > p2.Id;
+
+
+DELETE FROM Person WHERE Id NOT IN
+(SELECT * FROM(
+    SELECT MIN(Id) FROM Person GROUP BY Email) as p);
+
+
+
